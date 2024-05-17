f76a996f15f522fe1c788468381f75954aa84f72
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2024-05-16 22:09:17 -0300

    Instalação dos pacotes e criação da classe Usuario

---
+++ MVCProject/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCProject.Models
+{
+    [Table("TB_USUARIO")]
+    public class Usuario
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+
+    
+}
